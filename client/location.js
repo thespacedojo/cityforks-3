57c6b1ac89cce370894522bba7b6888b559a5f55
Josh Owens <joshua.owens@gmail.com>
2015-04-15 10:31:10 -0400

    Get data filling in the db

---
+++ client/location.js
@@ -0,0 +1,8 @@
+Meteor.startup(function() {
+  navigator.geolocation.getCurrentPosition(success);
+});
+
+success = function(position) {
+  Session.set('location', position);
+  Meteor.call('fetchNearbyLocations', position.coords);
+};
